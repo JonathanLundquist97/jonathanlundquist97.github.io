db671400840775a810842fb619645a48602e030c
JonathanLundquist97 <55597694+JonathanLundquist97@users.noreply.github.com>
2019-11-15 18:01:13 -0700

    Add files via upload

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -1,32 +1,37 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
     //console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const prophets = jsonObject['prophets'];
-    for (let i = 0; i < prophets.length; i++ ) {
+    const towns = jsonObject['towns'];
+    for (let i = 1; i <= 5; i++ ) { 
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
-        let a = document.createElement('p');
+        let a = document.createElement('h4');
         let b = document.createElement('p');
+        let c = document.createElement('p');
+        let d = document.createElement('p');
         let image = document.createElement('img');
 
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        a.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-        b.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-        image.setAttribute('src', prophets[i].imageurl);
+          h2.textContent = towns[i].name;
+          a.textContent = towns[i].motto;
+          b.textContent = 'Year Founded: '+ towns[i].yearFounded;
+          c.textContent = 'Population: '+ towns[i].currentPopulation;
+          d.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+          image.setAttribute('src', towns[i].photo);
         
 
         
-        card.appendChild(h2);
-        card.appendChild(a);
-        card.appendChild(b);
-        card.appendChild(image);
+          card.appendChild(h2);
+          card.appendChild(a);
+          card.appendChild(b);
+          card.appendChild(c);
+          card.appendChild(d);
+          card.appendChild(image);
 
-        document.querySelector('div.cards').appendChild(card);
-
+          document.querySelector('div.cards').appendChild(card);
     }
 });
 
