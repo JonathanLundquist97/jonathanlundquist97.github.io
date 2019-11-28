02cd9d782445d76952a4a42e2f95e4ef6781ff9c
JonathanLundquist97 <55597694+JonathanLundquist97@users.noreply.github.com>
2019-11-28 12:17:23 -0700

    Add files via upload

---
+++ Lesson11/gallery/galleryscript.js
@@ -0,0 +1,12 @@
+// Gets the date.
+var monthArray = ["January","February","March", "April", "May","June","July","August","September", "October", "November","December"];
+var dayArray = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var theDate = new Date();
+var dayOfWeek = dayArray[theDate.getDay()];
+var monthName = monthArray[theDate.getMonth()];
+var finalDate = dayOfWeek+ " " + monthName + " " + theDate.getDate() +", "+ (theDate.getYear() + 1900);    
+document.getElementById("currentDate").innerHTML = finalDate;
+// Banner display if Saturday
+if (dayOfWeek == "Friday") {
+	document.getElementById("advertisementSpot").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."; 
+}
